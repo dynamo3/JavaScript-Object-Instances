92d38e9dfe7af8d50c4ec73aaf65cf90a9988b34
Brig Lamoreaux <bklamoreaux@gmail.com>
2014-06-02 11:14:06 -0700

    added new katas

--- main.js
+++ main.js
@@ -17,19 +17,51 @@ var monopoly = new Game({
 //-------------
 
 // Make a Reptile Object
+var Reptile = function(settings) {
+    console.log(settings);
+};
+
 
 // Instantiate a frog instance of the Reptile Object
 // and pass a variety of settings in via an Object Literal
-
+var frog =new Reptile({
+    name: 'Kermit',
+    habitat: 'Lake Pleasant'
+});
 
 //-------------
 
 // Make a Person Object that has several default properties
-
+var Person = function(setting){
+    
+    this.setting = $.extend(
+        {
+            'arms': 2,
+            'legs': 2,
+            'eyes': 2
+        },
+            setting);
+    
+    console.log(this);
+};
 // Instantiate a student instance of the Person Object
 // and pass a variety of settings in via an Object Literal
 // These settings should have the ability to override any
 // default settings
+var student = new Person({
+    name:"john",
+    legs: 1,
+    lastname: 'jim'
+});
+
+
+
+
+
+
+
+
+
 
 
 //-------------
@@ -42,8 +74,21 @@ var Cart = function() {
 	// Make a method here called addItem(item, qty) that takes
 	// two arguments (item and quantity). Calling this method
 	// should interact with the items property already mentioned
-
-}
+    this.addItem = function(item, qty){
+       
+        // If the item is found
+        if(this.items[item]){
+            this.items[item] = this.items[item] + qty;    
+        }
+        else
+        {
+            // Add a new item to my list with an initial value
+            this.items[item] =  qty;
+            
+        }
+        
+    };
+};
 
 
 // Instantiate a cart instance of the Cart Object (We won't
@@ -51,7 +96,85 @@ var Cart = function() {
 var myCart = new Cart();
 
 // Call the addItem method of my_cart and pass in values
-
+myCart.addItem('eggs', 12);
+myCart.addItem('milk', 2);
+myCart.addItem('eggs', 6);
 
 // Comment in this line of code to check your work
-//console.log( myCart.items );
+console.log( myCart );
+
+
+
+// KATA 1. 
+// 1. Make a Bug Object that has default settings 
+//      (x=0,y=0, color, legs)
+// 2. Add a Console.log(settings) in Bug
+// 3. Add a method to move(x,y)
+//      Update the location to a new location
+var Bug = function(settings){
+
+    this.settings = $.extend({
+        x:0,
+        y:0,
+        color:'black',
+        legs:6
+    }, settings);
+
+    console.log(this.settings);
+
+    this.move = function(x,y){
+        this.settings.x = x;
+        this.settings.y = y;
+    };
+
+};
+
+//
+// Instatiate a new Grasshopper and pass settings for
+// (legs and anything else you want)
+// 1. Call move(2,3)
+// 2. Console out Grasshopper 
+var grassHopper = new Bug({
+    color:'green',
+    name:'Jimminy'
+});
+
+grassHopper.move(2,3);
+
+console.log(grassHopper);
+
+
+
+
+
+
+
+
+
+
+// KATA 2. 
+// 1. Make a Game Object that has default settings 
+//      (title, startTime, participantCount=0)
+// 2. Add a method called add(qty)
+//      Update the number of participants
+//
+// Instatiate a new Monopoly Game and pass settings for
+// (title and anything else you want)
+// 1. Call add(2)
+// 2. Console out Monopoly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
